fdd896789c9d3d0ece15c162a8056feee9397dee
Ford Prior <ford.prior@snagajob.com>
2018-04-13 21:29:08 -0400

    Create app.rb

---
+++ src/app.rb
@@ -0,0 +1,23 @@
+require 'sinatra/base'
+
+class ObstacleConcourse < Sinatra::Base
+
+  get '/' do
+    'hello world'
+  end
+
+  get '/:text/:format' do
+    # matches "GET /hello/reversed" and "GET /goodbye"
+
+    case params['format']
+      when 'upcase'
+        "#{params['text'].upcase}"
+      when 'reversed'
+        "#{params['text'].reverse}"
+      else
+        "#{params['text']}"
+    end
+
+  end
+
+end
